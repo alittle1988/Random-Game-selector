eabba5cbdc53cedfb2ef1e5fbfc2b250ba4e44c1
Andrew <andylittle88@gmail.com>
2022-08-23 12:07:56 -0700

    creaded functions and even Listeners

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const gameArray = [];
-const addedGameList = document.querySelector('.addedGameList')
-const addGame = document.querySelector('.addGameBtn')
-const gameInput = document.querySelector('.addGameInput')
+const addedGameList = document.querySelector('.addedGameList');
+const addGame = document.querySelector('.addGameBtn');
+const gameInput = document.querySelector('.addGameInput');
+const selectGameBtn = document.getElementById('selectGameBtn')
+const selectedGameList = document.getElementById('selectedGameList')
 
 
-
+// game functions 
+// adds game to game list
 const addGameFunc = (input) => {
     gameArray.push(input)
     let game = '';
@@ -13,19 +16,37 @@ const addGameFunc = (input) => {
     }
     return game
     
-}
+};
 
-addGame.addEventListener('click', () => {
-    addedGameList.innerHTML = addGameFunc(gameInput.value)
-    gameInput.value = '';
-})
+// adds game to selected game list
 
-const selectGame = (list) => {
-    randNum = Math.floor(Math.random() * list.length)
-    if(addedGameList.length === 0){
-
-    } else {
+const addSelectedGame = () => {
+    let randNum = Math.floor(Math.random() * gameArray.length)
+    let selectedGame = '';
+    if(gameArray.length === 0){
+        alert('Please add games')
+    } else{
+        selectedGame += `<li>${gameArray[randNum]}</li>`;
+        gameArray.splice(randNum, 1)
         
     }
-
+    return selectedGame
 }
+
+
+// event listeners 
+
+addGame.addEventListener('click', () => {
+    if(gameInput.value === '') {
+        alert('Please enter a game!')
+    } else {
+    addedGameList.innerHTML = addGameFunc(gameInput.value)
+    gameInput.value = '';
+    }
+});
+
+selectGameBtn.addEventListener('click', () => {
+    selectedGameList.innerHTML += addSelectedGame()
+    console.log(gameArray)
+
+})
